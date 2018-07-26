acdcff312ce7719447dfc59c8f3651b593b05db6
bgongre <27456325+bgongre@users.noreply.github.com>
2018-07-26 09:16:22 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const addButton = document.getElementById('add-button');
+const blockTxt = document.getElementById('block-txt');
+// const encryptedTxt = CryptoJS.AES('message');
+// console.log(encryptedTxt);
+
+let block = 1;
+
+particlesJS.load('particles-js', 'particles.json');
+
+const todayDate = () => {
+  let today = new Date();
+  let utcDate = today.toUTCString();
+  return utcDate;
+}
+
+document.getElementById('date').innerHTML = todayDate();
+
+const addBlock = () => {
+  let itm = document.getElementById('box-wrapper');
+  let clone = itm.cloneNode(true);
+  itm.parentNode.insertBefore(clone, itm.nextSibling);
+}
+
+const changeBlockNum = () => {
+  blockTxt.innerHTML = 'Block #' + block++;
+}
+
+addButton.addEventListener('click',() => {
+  addBlock();
+  changeBlockNum();
+})
