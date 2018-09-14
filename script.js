dddd82a8f03a4f9f0a696c976649d7f9b7eeedb7
bgongre <27456325+bgongre@users.noreply.github.com>
2018-09-14 16:33:02 -0400

    Fixed previous input not disabling when 'Add Block' was hit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const nonce = document.getElementById('nonce-info');
 const allInput = document.querySelectorAll('input[type=text]');
 const date = document.getElementById('date');
 
-
+//particles.js object variables
 window.onload = function() {
   Particles.init({
     selector: '.background',
@@ -18,42 +18,57 @@ window.onload = function() {
   });
 };
 
+//variable for counting past Genesis block
 let block = 1;
+
+//previous hash set to 0 by default
 prevHash.value = 0;
 
-let encryptArr = [];
+//holds previous hash values, defaulted to null in the event Add Block is pressed before current block is mined
+let encryptArr = [null];
+
+//holds and updates nonce for blocks
 let nonceArr = [];
+
+//sets first nonce if mine button is pressed without input to encrypt
 nonceArr[0] = 2345;
 
-const todayDate = () => {
+//get the current date and time
+const getTodayDate = () => {
   let today = new Date();
   let utcDate = today.toUTCString();
   return utcDate;
 }
 
+//set the current date and time on the block(s)
 const setDate = () => {
-  date.innerHTML = todayDate();
+  date.innerHTML = getTodayDate();
 }
 
 setDate();
 
+//create new block and clear all previous data
 const addBlock = () => {
   let block = document.getElementById('box-wrapper');
   let clone = block.cloneNode(true);
   block.parentNode.insertBefore(clone, block.nextSibling);
+  dataTxt.disabled = false;
   clearInputData();
 }
 
+//clear block data
 const clearInputData = () => {
   dataTxt.value = '';
   hashTxt.value = '';
   nonce.value = '';
 }
 
+//increment block numbers when new block is added
 const changeBlockNum = () => {
   blockTxt.innerHTML = 'Block #' + block++;
 }
 
+//encrypt text input field
 const encryptData = (str) => {
   str = dataTxt.value;
   let encrypt = CryptoJS.SHA256(str);
@@ -61,14 +76,18 @@ const encryptData = (str) => {
   encryptArr.push(encryptStr);
 }
 
+//push encrypted text in to array
 const getHash = () => {
   encryptArr[0] = hashTxt.value;
 }
 
+//set previous encrypted string to current previous hash field
 const setPrevHash = () => {
-  prevHash.value = encryptArr[0];
+    prevHash.value = encryptArr[0];
+    encryptArr[0] = null;
 }
 
+//calculate the nonce (number only used once)
 const calcNonce = () => {
   str = '';
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -79,16 +98,14 @@ const calcNonce = () => {
   return str;
 }
 
+//assign calculated nonce in nonceArr
 const setNonce = () => {
   nonceArr[0] = calcNonce();
 }
 
+//set calculated nonce in array to nonce DOM element
 const placeNonce = () => {
   nonce.value = nonceArr[0];
-}
-
-const disableInput = () => {
-  allInput.disabled = true;
 }
 
 mineButton.addEventListener('click', () => {
@@ -100,11 +117,10 @@ mineButton.addEventListener('click', () => {
 dataTxt.addEventListener('input', setNonce);
 
 addButton.addEventListener('click', () => {
+  dataTxt.disabled = true;
   addBlock();
-  clearInputData();
   setPrevHash();
   changeBlockNum();
   setDate();
   setNonce();
-  disableInput();
 })
